50c7f2090f8804a4775999d37f04f4a326bcd30e
Joseph Meleshko <jmeleshk@ualberta.ca>
2019-01-19 15:36:27 -0700

    starting point for main

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,32 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
 int main(int argc, char** argv) {
+    if (!glfwInit()) {
+        std::cout << "FAILED!";
+        return 1;
+    } else {
+        std::cout << "Initilized";
+    }
+    
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Engine_Title_Placeholder", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Window failed";
+        return 1;
+    }
+    else {
+        std::cout << "Window created";
+    }
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
+    
+    while(true){};
+    //shutdown protocol
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
 }
