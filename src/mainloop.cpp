697e37db7031500c91cbc9cd9f24b66f9fa3fe9a
Jacob Paton <jacobpaton@shaw.ca>
2019-01-19 19:30:54 -0700

    So you apparently need to actually call functions for them to work

--- src/mainloop.cpp
+++ src/mainloop.cpp
@@ -1,4 +1,5 @@
 #include "mainloop.h"
+#include "drawing.h"
 #include <GLFW/glfw3.h>
 
 void mainLoop(GLFWwindow* window) {
@@ -6,6 +7,7 @@ void mainLoop(GLFWwindow* window) {
         // Clear the screen
         glClear( GL_COLOR_BUFFER_BIT );
 
+        draw();
         // Swap buffers and get events
         glfwSwapBuffers(window);
         glfwPollEvents();
