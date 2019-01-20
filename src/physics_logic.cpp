be81ea566812ac86b5eb27426a8efa270257c9e6
Patrisha <ttdeboon@gmail.com>
2019-01-19 19:33:06 -0700

    Started Physics Logic

--- src/physics_logic.cpp
+++ src/physics_logic.cpp
@@ -1,9 +1,11 @@
-#include <glml/vec3.hpp>
 #include "DataStructures.h"
 
 void CheckCollisionType(Shape *shape1, Shape *shape2){
-	
+	if (shape1->getWhoAmI() == 1 && shape2->getWhoAmI() == 1) {
+        CollideSpheres((Sphere*)shape1, (Sphere*)shape2);
+    }
+}
 
-
-
+void CollideSpheres(Sphere *sphere1, Sphere *sphere2) {
+    int temp = sphere1->getWhoAmI();
 }
